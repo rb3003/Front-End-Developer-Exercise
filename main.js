1b30e2f133a6a70712721fb49bf3e2203c0e4290
rb3003 <rballen@insight.rr.com>
2016-01-30 14:36:53 -0500

    Created list of tasks

---
+++ main.js
@@ -0,0 +1,23 @@
+main ={};
+
+var task = function(name, date, assignee){
+    return {"name": name, "date": date, "assigned": assignee
+    };
+};
+
+main.tasks = [
+    new task("Test Task #1", "12/01/2012", "John Doe" ),
+    new task("Test Task #2", "12/02/2012", "John Doe" ),
+    new task("Test Task #3", "12/03/2012", "John Doe" ),
+    new task("Test Task #4", "12/04/2012", "John Doe" ),
+    new task("Test Task #5", "12/05/2012", "John Doe" ),
+    new task("Test Task #6", "12/06/2012", "John Doe" ),
+    new task("Test Task #7", "12/07/2012", "John Doe" )
+];
+
+
+$( document ).ready(function() {
+    main.tasks.forEach(function (task){
+        $('#taskTable tbody:last-child').append("<tr><td>" + task.name +"</td><td class'pull-left'>" + task.date +"</td><td>" + task.assigned +"</td></tr>");
+    });
+});
